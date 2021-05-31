0c03488f7eb36366e2c1b1d3253c471e840d8c21
Bennyyoung <benjamineffiong124@yahoo.com>
2021-05-31 13:09:27 +0100

    Fixing the signup disabled issue problem which was due to logic issue

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -114,7 +114,7 @@ export const Signup = () => {
               </span>
             </div>
 
-            <button disabled={isSubmitting || !isValid} type="submit">
+            <button disabled={!isValid || isSubmitting} type="submit">
               Sign Up
             </button>
           </Form>
